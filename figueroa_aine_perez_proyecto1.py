13e20201a07e16e441a5daac94ce679f90d2eca3
Ainé Figueroa <aine.xena.100@gmail.com>
2024-11-04 15:09:37 +0100

    Funciona código base con los nombres bien asignados; creados ficheros de prueba

--- figueroa_aine_perez_proyecto1.py
+++ figueroa_aine_perez_proyecto1.py
@@ -1,15 +1,23 @@
 def num_aleatorio() -> int:
+    '''
+    Genera y devuelve un número aleatorio entre 0 y 2 (ambos incluídos)
+    '''
     import random
     return random.randint(0, 2)
 
 def menu() -> str:
+    '''
+    Función que genera un menú de presentación antes de comenzar el juego.
+    Pregunta al usuario si quiere conocer las reglas para mostrar más información
+    '''
     print("Bienvenid@ al juego, a continuación mostraremos una breve explicación del funcionamiento y el menú de opciones.")
     print("Piedra gana a tijera; tijera gana a papel; papel gana a piedra.")
+    
     # print("Piedra gana a tijera y a lagarto; tijera gana a papel y a lagarto; papel gana a piedra y a spock; lagarto gana a spock y a papel; spock gana a tijeras y a piedra.")
     print("""
-    0. Piedra   // gana a tijera
-    1. Papel    // gana a piedra
-    2. Tijeras   // gana a papel
+    1. Piedra
+    2. Papel
+    3. Tijeras
     """)
     # 1 > 3 // gana ordenador
     # 1 < 2
@@ -25,6 +33,13 @@ def menu() -> str:
         #   """)
 
         # para lagart-spock usamos %5 porque cada 1 gana a los 2 siguientes
+
+    respuesta = input("¿Quiere ver las reglas del juego? (s/n) ")
+    while respuesta != "s" and respuesta != "n":
+        print('El parámetro introducido no es válido, por favor inténtelo de nuevo respondiendo con "s" o "n"')
+        respuesta = input("¿Quiere ver las reglas del juego? (s/n) ")
+    print()
+    # continuar más adelante con esto
 
 def eleccion(numero: int) -> str:
     match numero:
@@ -43,7 +58,7 @@ def eleccion(numero: int) -> str:
 def partida(ordenador: int, usuario: int) -> bool | str:
     if ordenador == usuario:
         return "empate"
-    elif ordenador == (usuario +1) % 3:
+    elif ordenador == (usuario +1) % 3: # Con esta fórmula gana el ordenador
         return True
     return False
 
@@ -53,15 +68,16 @@ victorias_ordenador = 0
 menu()
 
 while victorias_ordenador < 3 and victorias_usuario < 3:
-    opcion_usuario = int(input("Elija una de las opciones: "))
+    opcion_usuario = int(input("Elija una de las opciones: ")) - 1
 
     while opcion_usuario < 0 or opcion_usuario > 2:
-        print("El parámetro introducido no es válido, debe introducir un número entero comprendido entre 0 y 2, por favor inténtelo de nuevo.")
-        opcion_usuario = int(input("Elija una de las opciones: "))
+        print("El parámetro introducido no es válido, debe introducir un número entero comprendido entre 1 y 3, por favor inténtelo de nuevo.\n")
+        opcion_usuario = int(input("Elija una de las opciones: ")) - 1
 
     opcion_ordenador = num_aleatorio()
+    # eleccion_ordenador = opcion_ordenador + 1
 
-    print("Tu eleccion ha sido", eleccion(opcion_usuario), "y la del ordenador", eleccion(opcion_ordenador), num_aleatorio())
+    print("Tu eleccion ha sido", eleccion(opcion_usuario), "y la del ordenador", eleccion(opcion_ordenador))
 
     resultado = partida(opcion_ordenador, opcion_usuario)
 
